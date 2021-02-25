bdedf29cbb7acbcbc5cb630597d09f2280d2e292
phelixdusengimana <phelixdusengimana@gmail.com>
2021-02-25 11:40:53 +0200

    browse mv

--- index.php
+++ index.php
@@ -1,44 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agasobanuye-</title>
-    <link rel="stylesheet" href="css/uikit-rtl.min.css">
-    <link rel="stylesheet" href="css/uikit.min.css">
+    <title>browsemv-online movie searching made easier.</title>
 </head>
 <body>
-<nav class="uk-navbar-container uk-margin" uk-navbar>
-    <div class="uk-navbar-left">
-
-        <a class="uk-navbar-item uk-logo nkjsd" href="#">Logo</a>
-
-        <ul class="uk-navbar-nav">
-            <li>
-                <a href="#">
-                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
-                    Features
-                </a>
-            </li>
-        </ul>
-
-        <div class="uk-navbar-item">
-            <div>Some <a href="#">Link</a></div>
-        </div>
-
-        <div class="uk-navbar-item">
-            <form action="javascript:void(0)">
-                <input class="uk-input uk-form-width-small" type="text" placeholder="Input">
-                <button class="uk-button uk-button-default">Button</button>
-            </form>
-        </div>
-
-    </div>
-</nav>
-
-
-<script src="js/uikit-icons.min.js"></script>
-<script src="js/uikit.min.js"></script>
+    
 </body>
 </html>
