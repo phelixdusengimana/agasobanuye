b34432c6ee0b9106c7afca3600c14e90d62fce4b
phelixdusengimana <phelixdusengimana@gmail.com>
2021-02-25 11:48:23 +0200

    config requireds

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>browsemv-online movie searching made easier.</title>
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     
+    <script src="js/external/jquery/jquery.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
