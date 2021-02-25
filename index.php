fab19688997443ffa8ad13b15db03c523ec108fd
phelixdusengimana <phelixdusengimana@gmail.com>
2021-02-25 11:58:28 +0200

    datas

--- index.php
+++ index.php
@@ -10,8 +10,25 @@
 </head>
 <body>
     
+<h2 class="demoHeaders">Accordion</h2>
+<div id="accordion">
+	<div>First</div>
+	<div>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
+	<h3>Second</h3>
+	<div>Phasellus mattis tincidunt nibh.</div>
+	<h3>Third</h3>
+	<div>Nam dui erat, auctor a, dignissim quis.</div>
+</div>
+
+<h2 class="demoHeaders">Dialog</h2>
+<p>
+	<button id="dialog-link" class="ui-button ui-corner-all ui-widget">
+		<span class="ui-icon ui-icon-newwin"></span>Open Dialog
+	</button>
+</p>
     <script src="js/external/jquery/jquery.js"></script>
     <script src="js/jquery-ui.js"></script>
     <script src="js/scripts.js"></script>
+</script>
 </body>
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$( "#accordion" ).accordion();
+$( "#dialog" ).dialog({
+	autoOpen: false,
+	width: 400,
+	buttons: [
+		{
+			text: "Ok",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		},
+		{
+			text: "Cancel",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	]
+});
+// Link to open the dialog
+$( "#dialog-link" ).click(function( event ) {
+	$( "#dialog" ).dialog( "open" );
+	event.preventDefault();
+});
